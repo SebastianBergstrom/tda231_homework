e399ac3ba326cf829100ff01bbf11baaf1dcbaf1
SebastianBergstrom <sebbergs@student.chalmers.se>
2018-05-04 12:55:01 +0200

    småfix

--- hw3/grad_report.m
+++ hw3/grad_report.m
@@ -1,5 +1,4 @@
-%% TODO - Write code here ---------------
-    % Right now the function just returns a lot of zeros. Your job is to change that.
+  %% Computation of gradients
     function res = logistic_derivative(input)
         res = exp(-input) ./ (1 + exp(-input)).^2;
     end
@@ -7,5 +6,5 @@
                         1/size(data.inputs,2) * model.hid_to_class' * (class_prob-data.targets) ...
                        .* logistic_derivative(model.input_to_hid*data.inputs)  * data.inputs';
     res.hid_to_class = wd_coefficient*model.hid_to_class + ...
-                       1/size(data.inputs,2) * (class_prob-data.targets) * logistic(model.input_to_hid * data.inputs)';
-  % ---------------------------------------
+                       1/size(data.inputs,2) * (class_prob-data.targets) ...
+                       * logistic(model.input_to_hid * data.inputs)';
--- hw3/net.m
+++ hw3/net.m
@@ -138,8 +138,8 @@ function res = grad(model, data, wd_coefficient)
                         1/size(data.inputs,2) * model.hid_to_class' * (class_prob-data.targets) ...
                        .* logistic_derivative(model.input_to_hid*data.inputs)  * data.inputs';
     res.hid_to_class = wd_coefficient*model.hid_to_class + ...
-                       1/size(data.inputs,2) * (class_prob-data.targets) * logistic(model.input_to_hid * data.inputs)';
-  % ---------------------------------------
+                       1/size(data.inputs,2) * (class_prob-data.targets) ...
+                       * logistic(model.input_to_hid * data.inputs)';
 end
 
 %% Activation functions
