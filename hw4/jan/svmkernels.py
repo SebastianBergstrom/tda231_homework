cc0d89780a26273ba782202dad5d02aa6f72e876
fsharpasharp <ppooii@gmail.com>
2018-05-09 10:09:35 +0200

    Added support vector

--- hw4/jan/svmkernels.py
+++ hw4/jan/svmkernels.py
@@ -20,9 +20,14 @@ plt.plot(X[classA,0], X[classA,1], 'o', label='Class +1', markersize=mSize)
 plt.plot(X[~classA,0], X[~classA,1], 'o', label='Class -1', markersize=mSize)
 predictions = clf.predict(X)
 wronglyLabeled = predictions != Y
+
+
 plt.plot(X[wronglyLabeled, 0], X[wronglyLabeled, 1], 'o',
          markerFaceColor='none', markeredgewidth=edgeWidth,
          markerSize=mSize+edgeWidth, label='Failed to classify correctly')
+plt.plot(X[clf.support_, 0], X[clf.support_, 1], 'o',
+         markerFaceColor='none', markeredgewidth=edgeWidth,
+         markerSize=mSize+edgeWidth, label='Support vector')
 x = np.linspace(min(X[:,0])-padding, max(X[:,0])+padding)
 plt.plot(x, f(x, *clf.coef_[0], clf.intercept_))
 plt.legend()
