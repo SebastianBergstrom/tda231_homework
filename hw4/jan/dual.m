75ddde0c4998189c86479c3bdbaedbdb2b1d5a32
fsharpasharp <ppooii@gmail.com>
2018-05-14 17:24:39 +0200

    Added missing factor

--- hw4/jan/dual.m
+++ hw4/jan/dual.m
@@ -3,3 +3,13 @@ t = [1,1,1,-1,-1,-1];
 x = [2,2;4,4;4,0;0,0;2,0;0,2];
 N = length(t);
 alpha = fmincon(@(alpha) -f(alpha, t, x), zeros(N,1), zeros(1,N), 0, t, 0, zeros(N,1))
+
+
+%%
+s = 0;
+for i = 1:6
+  s = s + alpha(i)*t(i)*x(i,:);
+end
+
+s
+%%
