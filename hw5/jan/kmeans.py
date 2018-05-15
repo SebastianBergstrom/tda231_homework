e64ca8eaca6b79b9a21d6bb1d368f49cee7b0bdd
fsharpasharp <ppooii@gmail.com>
2018-05-15 13:45:24 +0200

    b) part done

--- hw5/jan/kmeans.py
+++ hw5/jan/kmeans.py
@@ -4,14 +4,16 @@ import scipy.io
 import numpy as np
 import matplotlib.pyplot as plt
 
-def kmeans(k, X, plot=False):
+def kmeans(k, X):
     assert(k >= 1)
 
     mu = np.random.rand(k, 2)
-    if plot:
-        point, = plt.plot(mu[:,0], mu[:,1], 'o')
+
+    it = 0
 
     while True:
+        if it == 2:
+            assignmentsTwoIterations = indices
         distances = np.linalg.norm(mu[0,:]-X, axis=1)
         indices = np.zeros(X.shape[0])
         for i in range(1,k):
@@ -27,19 +29,23 @@ def kmeans(k, X, plot=False):
             muTemp[i,:] = sum(X[assigned])/np.count_nonzero(assigned)
 
         if (mu == muTemp).all():
+            changedAssignment = indices != assignmentsTwoIterations
+            plt.plot(X[changedAssignment, 0], X[changedAssignment, 1], 'o',
+                     label='Has changed assignment', markerfacecolor='none')
+            for i in range(k):
+                assigned = indices == i
+                plt.plot(mu[i,0], mu[i,1], 'o', label='Cluster #{} mean'.format(i+1))
+                plt.plot(X[assigned, 0], X[assigned, 1], '.',
+                         label='Cluster #{}'.format(i+1))
+            plt.legend()
             return
 
         mu = muTemp
-        if plot:
-            point.set_xdata(mu[:,0])
-            point.set_ydata(mu[:,1])
-            plt.draw()
-            plt.pause(0.15)
+        it += 1
 
 
 
 mat = scipy.io.loadmat('hw5_p1a.mat')
 X = mat['X']
-plt.plot(X[:,0], X[:,1] , '.')
-kmeans(6, X, True)
+kmeans(2, X)
 ##
